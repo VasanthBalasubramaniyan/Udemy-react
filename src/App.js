8c397a4248e05399aba96b7b26b281d18d0415b1
VasanthBalasubramaniyan <vambuvasanth124@gmail.com>
2025-01-17 13:09:44 +0530

    Created react app and updated navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+import Categories from "./components/Categories";
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Categories></Categories>
+    </div>
+  );
+}
+
+export default App
